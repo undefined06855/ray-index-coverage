409bf2370615bd8b67dcac2a5931ca01c0698902
undefined06855 <currentcashew57@gmail.com>
2024-04-14 14:02:44 +0100

    bunch of changes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// who needs ids when you have document.querySelector?
 let canvas = document.querySelector("canvas")
   , ctx = canvas.getContext("2d")
   /** @type HTMLDivElement */
@@ -8,26 +7,45 @@ let canvas = document.querySelector("canvas")
   /** @type HTMLParagraphElement */
   , detailedExplanation = document.querySelector("p")
 
+  // stuff for pi chart animation
+  , animStage = 0
+  , animLength = 25000
+
+  , wikiHighlightLink = "https://en.wikipedia.org/wiki/Floating-point_arithmetic#Representable_numbers,_conversion_and_rounding:~:text=The%20number%20of%20digits%20(or%20bits)%20of%20precision%20also%20limits%20the%20set%20of%20rational%20numbers%20that%20can%20be%20represented%20exactly.%20For%20example%2C%20the%20decimal%20number%20123456789%20cannot%20be%20exactly%20represented%20if%20only%20eight%20decimal%20digits%20of%20precision%20are%20available"
+
+  // the iife is literally just for the await later on
 !(async () => {
     let rayModStats = await getRayModStats()
     percentEl.innerText = rayModStats.percentage.toFixed(3) + "%"
-    percentageSpecificEl.innerHTML = `(Or more specifically, <strong>${rayModStats.percentage}%</strong>)`
+    percentageSpecificEl.innerHTML = `(Or more specifically, <a href="${wikiHighlightLink}" class="no-deco" target="_blank"><strong>${rayModStats.percentage}%</strong>*</a>)`
     detailedExplanation.innerHTML = `That's <strong>${rayModStats.eryMods}</strong> out of the <strong>${rayModStats.totalMods}</strong> mods on the index!`
 
-    // nice graph
-    let center = canvas.width / 2
+    // me when i can't use CCEaseOut on web:
+    function tickAnim() {
+        animStage += (animLength - animStage) / 16
+        let multiplier = animStage / animLength
 
-    ctx.fillStyle = "#00bfbf"
-    ctx.beginPath()
-    ctx.moveTo(center, center)
-    ctx.arc(center, center, center, -1.5708, Math.PI * 2 * rayModStats.drawablePercentage - 1.5708)
-    ctx.lineTo(center, center)
-    ctx.fill()
+        // nice graph
+        let center = canvas.width / 2
 
-    ctx.fillStyle = "#6a5b9b"
-    ctx.beginPath()
-    ctx.moveTo(center, center)
-    ctx.arc(center, center, center, Math.PI * 2 * rayModStats.drawablePercentage - 1.5708, -1.5708)
-    ctx.lineTo(center, center)
-    ctx.fill()
+        ctx.fillStyle = "#00bfbf"
+        ctx.beginPath()
+        ctx.moveTo(center, center)
+        ctx.arc(center, center, center, -1.5708, (Math.PI * 2 * rayModStats.drawablePercentage * multiplier) - 1.5708)
+        ctx.lineTo(center, center)
+        ctx.fill()
+
+        ctx.fillStyle = "#6a5b9b"
+        ctx.beginPath()
+        ctx.moveTo(center, center)
+        ctx.arc(center, center, center, (Math.PI * 2 * rayModStats.drawablePercentage * multiplier) - 1.5708, -1.5708)
+        ctx.lineTo(center, center)
+        ctx.fill()
+
+        if (animLength == animStage) return
+
+        requestAnimationFrame(tickAnim)
+    }
+
+    tickAnim()
 })()
